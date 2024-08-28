ad239500185e7d8bee834722a29ca00ee71b59d7
silasener <slaasener@gmail.com>
2024-08-28 19:11:52 +0300

    fixed

--- src/main/java/tr/com/utils/StringUtils.java
+++ src/main/java/tr/com/utils/StringUtils.java
@@ -18,18 +18,4 @@ public class StringUtils {
         return !isBlank(str);
     }
 
-
-    public static String generateRandomString(int targetStringLength) {
-        int leftLimit = 48; //numeral '0'
-        int rightLimit = 122; // letter 'z'
-        Random random = new Random();
-
-        return random
-                .ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-    }
-
 }
